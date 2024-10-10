13d3adb14a38150c54707a28e4d996d04a3f65bd
Brice Copy <500789+bcopy@users.noreply.github.com>
2024-10-10 23:34:38 +0200

    Add id and types to device and nodes, use id instead of name for lookups

--- src/HomieDevice.ts
+++ src/HomieDevice.ts
@@ -3,18 +3,18 @@ import { HomieNode } from './HomieNode';
 export class HomieDevice {
   private nodes: Map<string, HomieNode> = new Map();
 
-  constructor(public name: string) {}
+  constructor(public id: string, public name: string = id, public type: string = '') { }
 
   addNode(node: HomieNode) {
-    this.nodes.set(node.name, node);
+    this.nodes.set(node.id, node);
   }
 
   removeNode(node: HomieNode){
-    this.nodes.delete(node.name);
+    this.nodes.delete(node.id);
   }
 
-  getNode(name: string): HomieNode | undefined {
-    return this.nodes.get(name);
+  getNode(id: string): HomieNode | undefined {
+    return this.nodes.get(id);
   }
 
   getAllNodes(): HomieNode[] {
--- test/src/CoralBranch.ts
+++ test/src/CoralBranch.ts
@@ -4,8 +4,8 @@ import { HomieProperty } from '../../src/HomieProperty';
 export class CoralBranch extends HomieNode {
   constructor(name: string) {
     super(name);
-    this.addProperty(new HomieProperty('color', 'pink', 'string'));
-    this.addProperty(new HomieProperty('length', 10, 'integer'));
+    this.addProperty(new HomieProperty('color', 'Color',  'pink', 'string'));
+    this.addProperty(new HomieProperty('length', 'Length', 10, 'integer'));
   }
 
   // Add coral branch specific methods here
